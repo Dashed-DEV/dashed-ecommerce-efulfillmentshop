ff546baf00612f7c1324d4ac9eac5efd22eda75b
Robin van Maasakker <robin@qubiqx.com>
2022-01-02 13:34:05 +0100

    Update

--- src/Commands/PushProductsToEfulfillmentShopCommand.php
+++ src/Commands/PushProductsToEfulfillmentShopCommand.php
@@ -40,7 +40,7 @@ class PushProductsToEfulfillmentShopCommand extends Command
     public function handle()
     {
         if (EfulfillmentShop::isConnected()) {
-            foreach (Product::thisSite()->get() as $product) {
+            foreach (Product::publicShowable()->get() as $product) {
                 EfulfillmentShop::pushProduct($product);
             }
         }
