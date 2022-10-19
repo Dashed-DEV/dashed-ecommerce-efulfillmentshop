fe2db6f114b38df886ddc1dfe798d946215c119c
Robinvm <Robinvm@users.noreply.github.com>
2022-10-19 10:44:37 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,8 @@
 
 namespace Qubiqx\QcommerceEcommerceEfulfillmentshop\Tests;
 
-use Orchestra\Testbench\TestCase as Orchestra;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as Orchestra;
 use Qubiqx\QcommerceEcommerceEfulfillmentshop\QcommerceEcommerceEfulfillmentshopServiceProvider;
 
 class TestCase extends Orchestra
