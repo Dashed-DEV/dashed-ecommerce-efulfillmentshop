683b95a6317d6fee22919854ae999cd15f7a3ea9
Robinvm <Robinvm@users.noreply.github.com>
2023-07-15 12:33:39 +0000

    Fix styling

--- src/Classes/EfulfillmentShop.php
+++ src/Classes/EfulfillmentShop.php
@@ -266,7 +266,7 @@ class EfulfillmentShop
             }
 
             if (! isset($status)) {
-//                throw new \Exception('No status found for efulfillmentOrder ' . $efulfillmentOrder->id);
+                //                throw new \Exception('No status found for efulfillmentOrder ' . $efulfillmentOrder->id);
 
                 return;
             }
