b8d278ed715e239f1dc0a672eaec64a743d385bb
Robin van Maasakker <robin@qubiqx.com>
2024-03-18 08:50:31 +0100

    Update

--- src/Filament/Widgets/EfulfillmentShopOrderStats.php
+++ src/Filament/Widgets/EfulfillmentShopOrderStats.php
@@ -2,18 +2,17 @@
 
 namespace Dashed\DashedEcommerceEfulfillmentshop\Filament\Widgets;
 
-use Dashed\DashedEcommerceEfulfillmentshop\Models\EfulfillmentshopOrder;
 use Filament\Widgets\StatsOverviewWidget;
-use Filament\Widgets\StatsOverviewWidget\Card;
+use Dashed\DashedEcommerceEfulfillmentshop\Models\EfulfillmentshopOrder;
 
 class EfulfillmentShopOrderStats extends StatsOverviewWidget
 {
     protected function getCards(): array
     {
         return [
-            Card::make('Aantal bestellingen naar E-fulfillment shop', EfulfillmentshopOrder::where('pushed', 1)->count()),
-            Card::make('Aantal bestellingen in de wacht', EfulfillmentshopOrder::where('pushed', 0)->count()),
-            Card::make('Aantal bestellingen gefaald', EfulfillmentshopOrder::where('pushed', 2)->count()),
+            StatsOverviewWidget\Stat::make('Aantal bestellingen naar E-fulfillment shop', EfulfillmentshopOrder::where('pushed', 1)->count()),
+            StatsOverviewWidget\Stat::make('Aantal bestellingen in de wacht', EfulfillmentshopOrder::where('pushed', 0)->count()),
+            StatsOverviewWidget\Stat::make('Aantal bestellingen gefaald', EfulfillmentshopOrder::where('pushed', 2)->count()),
         ];
     }
 }
