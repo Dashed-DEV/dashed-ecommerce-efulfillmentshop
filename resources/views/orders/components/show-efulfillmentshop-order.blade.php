988fbfb7423842a2c8f9a23fd4a6eb97f6f985c4
Robin van Maasakker <robin@qubiqx.com>
2024-10-10 12:40:29 +0200

    Update

--- resources/views/orders/components/show-efulfillmentshop-order.blade.php
+++ resources/views/orders/components/show-efulfillmentshop-order.blade.php
@@ -1,28 +1,29 @@
-<form wire:submit.prevent="submit">
+<form wire:submit.prevent="submit" class="grid gap-2">
     @if($order->efulfillmentShopOrder)
         @if($order->efulfillmentShopOrder->pushed == 1)
             <span
-                class="bg-green-100 text-green-800 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium">
+                class="w-full justify-center bg-green-100 text-green-800 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium">
                                 Bestelling naar E-fulfillment Shop gepushed
                                 </span>
         @elseif($order->efulfillmentShopOrder->pushed == 2)
             <span
-                class="bg-red-100 text-red-800 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium">
+                class="w-full justify-center bg-red-100 text-red-800 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium">
                                 Bestelling niet naar E-fulfillment Shop gepushed (Fout: {{ $order->efulfillmentShopOrder->error }})
                                 </span>
             <button type="submit"
-                    class="inline-flex items-center justify-center font-medium tracking-tight rounded-lg focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 h-9 px-4 text-white shadow focus:ring-white w-full mt-2">
-                Opnieuw naar E-fulfillment Shop pushen
+
+                    style="--c-400:var(--primary-400);--c-500:var(--primary-500);--c-600:var(--primary-600);"
+                    class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50 fi-ac-action fi-ac-btn-action">                Opnieuw naar E-fulfillment Shop pushen
             </button>
         @else
             <span
-                class="bg-yellow-100 text-yellow-800 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium">
+                class="w-full justify-center bg-yellow-100 text-yellow-800 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium">
                                 Bestelling wordt naar E-fulfillment Shop gepushed
                                 </span>
         @endif
         @else
         <span
-            class="bg-yellow-100 text-yellow-800 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium">
+            class="w-full justify-center bg-yellow-100 text-yellow-800 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium">
                                 Bestelling niet gekoppeld aan E-fulfillment shop
                                 </span>
     @endif
